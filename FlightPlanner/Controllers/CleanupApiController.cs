1521ab76bfe1d478e8829340defe09192f6bd3f4
Kympu <eriks.boiko@gmail.com>
2023-09-22 01:33:38 +0300

    test 01 pass

---
+++ FlightPlanner/Controllers/CleanupApiController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FlightPlanner.Controllers
+{
+    [Route("testing-api")]
+    [ApiController]
+    public class CleanupApiController : ControllerBase
+    {
+        [Route("clear")]
+        [HttpPost]
+        public IActionResult GetActionClear()
+        {
+            return Ok();
+        }
+    }
+}
