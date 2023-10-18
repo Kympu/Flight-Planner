67f2ba0e4b8ad4c3dbbe74da97a07ca3b5883efb
Ēriks Boiko <30439375+Kympu@users.noreply.github.com>
2023-10-19 00:18:28 +0300

    Revert "Flight planner core"

--- FlightPlanner/Models/AirportRequest.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FlightPlanner.Models
-{
-    public class AirportRequest
-    {
-        public string Country { get; set; }
-        public string City { get; set; }
-        public string Airport { get; set; }
-    }
-}
